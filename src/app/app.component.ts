0e2261b1466ed37b89c631b553b5dfe73332abcf
Bakal <d.bakal@controlpay.com>
2017-07-01 07:57:07 +0300

    Commit sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: ' <h1>My APP!!! {{title}}</h1> <toh-heroes></toh-heroes>',
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'appas';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HeroesComponent} from "./heroes/heroes.components";
+import {HeroComponent} from "./heroes/hero.components";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeroesComponent, HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
