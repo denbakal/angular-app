b68cbaaa4d66e3ae97c1fdedfc2f9be566c5b70d
Bakal <d.bakal@controlpay.com>
2017-07-15 23:39:55 +0300

    Using Dependency Injection for Mocking

--- src/test/shared/car.service.spec.ts
+++ src/test/shared/car.service.spec.ts
@@ -1,13 +1,28 @@
 import {CarService} from "../../app/shared/car.service";
 import {inject, TestBed} from "@angular/core/testing";
 import {EngineService} from "../../app/shared/engine.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+class V8Engine {
+  getHorsepower() {
+    return 400;
+  }
+
+  getName() {
+    return 'V8 engine';
+  }
+}
 
 describe('Car', () => {
   let subject: CarService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [EngineService, CarService]
+      providers: [
+        {provide: EngineService, useClass: V8Engine},
+        CarService
+      ]
     });
 
     spyOn(EngineService.prototype, 'getHorsepower').and.returnValue(400);
