fc4089f09d34fb20bd198052e3d0170bdcd672dc
Bakal <d.bakal@controlpay.com>
2017-07-15 21:12:13 +0300

    The karma config file was changed.

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,15 @@
 
 module.exports = function (config) {
   config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
+    // frameworks to use
     frameworks: ['jasmine', '@angular/cli'],
+
+    // list of files to exclude
+    exclude: [],
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,22 +19,44 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
+
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
+
     angularCli: {
       environment: 'dev'
     },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'kjhtml'],
+
+    // web server port
     port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-    browsers: ['Chrome'],
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'], // browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
 };
