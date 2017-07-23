183ea781518aafdaafe97904b408c7527f4c3a3a
Bakal <d.bakal@controlpay.com>
2017-07-23 16:05:09 +0300

    Making the test asynchronous

--- src/test/1st.spec.ts
+++ src/test/1st.spec.ts
@@ -1,3 +1,14 @@
+import {async} from "@angular/core/testing";
+
 describe('1st tests', () => {
   it('true is true', () => expect(true).toBe(true));
+});
+
+
+describe('asynchronous test', () => {
+  it('should be executed like asynchronous', async(() => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+    }, 2000);
+  }));
 });
